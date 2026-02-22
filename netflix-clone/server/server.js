fa19a97ef6b2fc3bbb24f1fb9cd8225344e86c57
todolist <ushakanadi123@gmail.com>
2026-02-22 21:51:51 +0530

    basic server

---
+++ netflix-clone/server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log("Connted to MongoDB"))
+.catch((err) => console.log(err));
+
+app.get("/",(req,res) => {
+    res.send("Ntflix clone API is running");
+});
+
+app.listen(5000, () => console.log("server is running on port 5000"));
